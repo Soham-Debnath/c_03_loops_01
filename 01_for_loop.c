e639eb0546fb07895cf6b6b3a8858ea3be45d7d2
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-13 19:16:24 +0530

    1_loop

---
+++ 01_for_loop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    for (int i =1; i<=5;i++){
+        printf("hello\n");
+    }
+    return 0;
+}
+// initialization->[[condition->statement/print->updation->condition...]]
